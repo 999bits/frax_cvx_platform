8fdaa6433eaa5c7e80ca852c2902c19b9efc1df7
rickuser9600 <rsimon9600@gmail.com>
2021-10-27 15:44:43 +0900

    add virtual reward pool

--- contracts/contracts/VirtualBalanceRewardPool.sol
+++ contracts/contracts/VirtualBalanceRewardPool.sol
@@ -11,7 +11,7 @@ pragma solidity 0.8.9;
 /___/ \_, //_//_/\__//_//_/\__/ \__//_/ /_\_\
      /___/
 
-* Synthetix: BaseRewardPool.sol
+* Synthetix: VirtualBalanceRewardPool.sol
 *
 * Docs: https://docs.synthetix.io/
 *
@@ -47,18 +47,16 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 
 
-contract BaseRewardPool {
+contract VirtualBalanceRewardPool {
     using SafeERC20 for IERC20;
-
+    
+    IDeposit public deposits;
     IERC20 public rewardToken;
-    IERC20 public stakingToken;
     uint256 public constant duration = 7 days;
     uint256 public constant newRewardRatio = 830;
 
     address public operator;
-    address public rewardManager;
 
-    uint256 public pid;
     uint256 public periodFinish;
     uint256 public rewardRate;
     uint256 public lastUpdateTime;
@@ -66,13 +64,8 @@ contract BaseRewardPool {
     uint256 public queuedRewards;
     uint256 public currentRewards;
     uint256 public historicalRewards;
-    
-    uint256 private _totalSupply;
     mapping(address => uint256) public userRewardPerTokenPaid;
     mapping(address => uint256) public rewards;
-    mapping(address => uint256) private _balances;
-
-    address[] public extraRewards;
 
     event RewardAdded(uint256 reward);
     event Staked(address indexed user, uint256 amount);
@@ -82,44 +75,23 @@ contract BaseRewardPool {
     constructor(){}
 
     function initialize(
-        uint256 pid_,
-        address stakingToken_,
-        address rewardToken_,
-        address operator_,
-        address rewardManager_
-    ) external {
-        require(address(stakingToken) == address(0),"already init");
+        address deposit_,
+        address reward_,
+        address op_
+    ) public {
+        require(address(deposits) == address(0),"already init");
 
-        pid = pid_;
-        stakingToken = IERC20(stakingToken_);
-        rewardToken = IERC20(rewardToken_);
-        operator = operator_;
-        rewardManager = rewardManager_;
+        deposits = IDeposit(deposit_);
+        rewardToken = IERC20(reward_);
+        operator = op_;
     }
 
     function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+        return deposits.totalSupply();
     }
 
     function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function extraRewardsLength() external view returns (uint256) {
-        return extraRewards.length;
-    }
-
-    function addExtraReward(address _reward) external returns(bool){
-        require(msg.sender == rewardManager, "!authorized");
-        require(_reward != address(0),"!reward setting");
-
-        extraRewards.push(_reward);
-        return true;
-    }
-
-    function clearExtraRewards() external{
-        require(msg.sender == rewardManager, "!authorized");
-        delete extraRewards;
+        return deposits.balanceOf(account);
     }
 
     modifier updateReward(address account) {
@@ -141,156 +113,46 @@ contract BaseRewardPool {
             return rewardPerTokenStored;
         }
         return rewardPerTokenStored + ((lastTimeRewardApplicable() - lastUpdateTime) * rewardRate * 1e18 / totalSupply());
-        // return
-        //     rewardPerTokenStored.add(
-        //         lastTimeRewardApplicable()
-        //             .sub(lastUpdateTime)
-        //             .mul(rewardRate)
-        //             .mul(1e18)
-        //             .div(totalSupply())
-        //     );
     }
 
     function earned(address account) public view returns (uint256) {
         return rewards[account] + (balanceOf(account) * (rewardPerToken() - userRewardPerTokenPaid[account]) / 1e18);
-        // return
-        //     balanceOf(account)
-        //         .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
-        //         .div(1e18)
-        //         .add(rewards[account]);
     }
 
-    function stake(uint256 _amount)
-        public
-        updateReward(msg.sender)
-        returns(bool)
+    //update reward, emit, call linked reward's stake
+    function stake(address _account, uint256 amount)
+        external
+        updateReward(_account)
     {
-        require(_amount > 0, 'RewardPool : Cannot stake 0');
-        
-        //also stake to linked rewards
-        for(uint i=0; i < extraRewards.length; i++){
-            IRewards(extraRewards[i]).stake(msg.sender, _amount);
-        }
-
-        _totalSupply += _amount;
-        _balances[msg.sender] += _amount;
-
-        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
-        emit Staked(msg.sender, _amount);
-
-        
-        return true;
+        require(msg.sender == address(deposits), "!authorized");
+       // require(amount > 0, 'VirtualDepositRewardPool: Cannot stake 0');
+        emit Staked(_account, amount);
     }
 
-    function stakeAll() external returns(bool){
-        uint256 balance = stakingToken.balanceOf(msg.sender);
-        stake(balance);
-        return true;
-    }
-
-    function stakeFor(address _for, uint256 _amount)
+    function withdraw(address _account, uint256 amount)
         public
-        updateReward(_for)
-        returns(bool)
+        updateReward(_account)
     {
-        require(_amount > 0, 'RewardPool : Cannot stake 0');
-        
-        //also stake to linked rewards
-        for(uint i=0; i < extraRewards.length; i++){
-            IRewards(extraRewards[i]).stake(_for, _amount);
-        }
+        require(msg.sender == address(deposits), "!authorized");
+        //require(amount > 0, 'VirtualDepositRewardPool : Cannot withdraw 0');
 
-        //give to _for
-        _totalSupply += _amount;
-        _balances[_for] += _amount;
-
-        //take away from sender
-        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
-        emit Staked(_for, _amount);
-        
-        return true;
+        emit Withdrawn(_account, amount);
     }
 
-
-    function withdraw(uint256 amount, bool claim)
-        public
-        updateReward(msg.sender)
-        returns(bool)
-    {
-        require(amount > 0, 'RewardPool : Cannot withdraw 0');
-
-        //also withdraw from linked rewards
-        for(uint i=0; i < extraRewards.length; i++){
-            IRewards(extraRewards[i]).withdraw(msg.sender, amount);
-        }
-
-        _totalSupply -= amount;
-        _balances[msg.sender] -= amount;
-
-        stakingToken.safeTransfer(msg.sender, amount);
-        emit Withdrawn(msg.sender, amount);
-     
-        if(claim){
-            getReward(msg.sender,true);
-        }
-
-        return true;
-    }
-
-    function withdrawAll(bool claim) external{
-        withdraw(_balances[msg.sender],claim);
-    }
-
-    function withdrawAndUnwrap(uint256 amount, bool claim) public updateReward(msg.sender) returns(bool){
-
-        //also withdraw from linked rewards
-        for(uint i=0; i < extraRewards.length; i++){
-            IRewards(extraRewards[i]).withdraw(msg.sender, amount);
-        }
-        
-        _totalSupply -= amount;
-        _balances[msg.sender] -= amount;
-
-        //tell operator to withdraw from here directly to user
-        IDeposit(operator).withdrawTo(pid,amount,msg.sender);
-        emit Withdrawn(msg.sender, amount);
-
-        //get rewards too
-        if(claim){
-            getReward(msg.sender,true);
-        }
-        return true;
-    }
-
-    function withdrawAllAndUnwrap(bool claim) external{
-        withdrawAndUnwrap(_balances[msg.sender],claim);
-    }
-
-    function getReward(address _account, bool _claimExtras) public updateReward(_account) returns(bool){
+    function getReward(address _account) public updateReward(_account){
         uint256 reward = earned(_account);
         if (reward > 0) {
             rewards[_account] = 0;
             rewardToken.safeTransfer(_account, reward);
-            IDeposit(operator).rewardClaimed(pid, _account, reward);
             emit RewardPaid(_account, reward);
-        }
-
-        //also get rewards from linked rewards
-        if(_claimExtras){
-            for(uint i=0; i < extraRewards.length; i++){
-                IRewards(extraRewards[i]).getReward(_account);
-            }
         }
 
         //check for new cycle
         _checkNewCycle();
-
-        return true;
     }
 
-    function getReward() external returns(bool){
-        getReward(msg.sender,true);
-        return true;
+    function getReward() external{
+        getReward(msg.sender);
     }
 
     function donate(uint256 _amount) external returns(bool){
