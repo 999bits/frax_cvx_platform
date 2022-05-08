673a0f54231d6b4acabe89fd09227d06ad30ba26
rickuser9600 <rsimon9600@gmail.com>
2022-05-08 10:51:23 +0900

    change addresses for ui tests

--- contracts/test/UI_2_VaultTest.js
+++ contracts/test/UI_2_VaultTest.js
@@ -198,9 +198,9 @@ contract("Vault Tests", async accounts => {
 
 
     ///create convex vault fpi/frax
-    let fpistakingToken = await IERC20.at("0xa6f93cd2cb774EC46ec8De0AEc43d7406ABc0413");
+    let fpistakingToken = await IERC20.at("0xA74A725F143C9b4cc139929b6Df67405f8eC9AAe");
     let fpilp = await IERC20.at("0x4704aB1fb693ce163F7c9D3A31b3FF4eaF797714");
-    let fpifarm = await TestPool_Erc20.new(fpilp.address);
+    let fpifarm = "0x183a756F416C723000E7BD3fB0e66cBA3E48c52D";
     let fpiHolder = "0xdb7cbbb1d5d5124f86e92001c9dfdc068c05801d";
     await unlockAccount(fpiHolder);
     await fpilp.transfer(userA,web3.utils.toWei("100000.0", "ether"),{from:fpiHolder,gasPrice:0});
@@ -212,7 +212,7 @@ contract("Vault Tests", async accounts => {
     await cvxReg.addPoolInfo(fpistakingToken.address, 82,{from:proxyAddress,gasPrice:0});
 
     let conveximpl = await StakingProxyConvex.new();
-    var tx = await booster.addPool(conveximpl.address, fpifarm.address, fpistakingToken.address,{from:multisig,gasPrice:0});
+    var tx = await booster.addPool(conveximpl.address, fpifarm, fpistakingToken.address,{from:multisig,gasPrice:0});
     console.log("convex fpi/frax pool added, gas: " +tx.receipt.gasUsed);
 
     var poolinfo = await poolReg.poolInfo(2);
