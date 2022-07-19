11601cbcd9d716f413bf9adecfd268760740052c
rickuser9600 <rsimon9600@gmail.com>
2022-07-19 11:31:45 +0900

    joint owner addresses

--- contracts/contracts/JointVaultManager.sol
+++ contracts/contracts/JointVaultManager.sol
@@ -11,10 +11,10 @@ import "./interfaces/IJointProxyVault.sol";
 contract JointVaultManager{
 
     address public constant owner = address(0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB);
-    address public constant jointowner = address(0x59CFCD384746ec3035299D90782Be065e466800B);
+    address public constant jointowner = address(0x8c2D06e11ca4414e00CdEa8f28633A2edAf79499);
 
     address public constant ownerProxy = address(0x59CFCD384746ec3035299D90782Be065e466800B);
-    address public constant jointownerProxy = address(0x59CFCD384746ec3035299D90782Be065e466800B);
+    address public constant jointownerProxy = address(0xc0223fb0562555bec938de5363d63edd65102283);
 
     uint256 public ownerIncentive = 200;
     uint256 public jointownerIncentive = 200;
