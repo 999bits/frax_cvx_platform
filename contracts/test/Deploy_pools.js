8255bdf9842e59a329043a2814de4e7ce13a0889
rickuser9600 <rsimon9600@gmail.com>
2022-12-14 13:54:19 +0900

    update deploy pool script

--- contracts/test/Deploy_pools.js
+++ contracts/test/Deploy_pools.js
@@ -92,8 +92,9 @@ contract("Vault Tests", async accounts => {
 
     const deployConvexPool = async (stakingAddress, targetname) => {
       var imp = contractList.system.vaultConvexImplementation;
-      console.log("\n-----------\n");
+      console.log("\n----- Deploy Convex Pool ------\n");
       console.log("farm for: " +targetname);
+      console.log("imp: " +imp);
       //get staking farm instance
       var farm = await IFraxFarmERC20.at(stakingAddress);
       console.log("staking address: " +farm.address);
@@ -143,21 +144,81 @@ contract("Vault Tests", async accounts => {
       })
     }
 
+    const deployERC20Pool = async (stakingAddress, targetname) => {
+      var imp = contractList.system.vaultErc20Implementation;
+      console.log("\n----- Deploy ERC20 Pool ------\n");
+      console.log("farm for: " +targetname);
+      console.log("imp: " +imp);
+      //get staking farm instance
+      var farm = await IFraxFarmERC20.at(stakingAddress);
+      console.log("staking address: " +farm.address);
 
-    await deployConvexPool("0xd1f21322bBDd3586dC1151ADCcaA2684641c2b31","Convex ageur/FraxBP");
-    await deployConvexPool("0xA0657642224Fc53dAB4a3d2069430afe157BEc5D","Convex alcx/FraxBP");
-    await deployConvexPool("0xeC670c5e0A8A8d5ae5639158565D840DE44CA03f","Convex CVX/FraxBP");
-    await deployConvexPool("0x57c9F019B25AaAF822926f4Cacf0a860f61eDd8D","Convex cvxCrv/FraxBP");
-    await deployConvexPool("0x2F9504988675c91787E188Ed928D6E042d9052e9","Convex cvxFxs/FraxBP");
-    await deployConvexPool("0xE7211E87D60177575846936F2123b5FA6f0ce8Ab","Convex dola/FraxBP");
-    await deployConvexPool("0xdE5684F85a78F6CcCFFB4b9301ad0944eb5CE3eE","Convex mai/FraxBP");
-    await deployConvexPool("0x40b42E4ab3c044e67CBFb0bD99C9E975dcB83668","Convex pusd/FraxBP");
+      //get stakingToken
+      var stakingToken = await farm.stakingToken();
+      var wrappertoken = await ERC20.at(stakingToken);
+      await wrappertoken.name().then(a=>console.log("token name: " +a))
+
+      //assert proxy
+      var proxy = await farm.getProxyFor(contractList.system.voteProxy);
+      console.log("proxy check: " +proxy);
+      // assert(proxy == voteproxy.address, "proxy not set yet!");
+
+      //add pool
+      await booster.addPool(imp, farm.address, stakingToken, {from:deployer});
+
+      var poolLength = await poolReg.poolLength();
+      console.log("pool added: " +(poolLength-1) );
+
+      var poolinfo = await poolReg.poolInfo(poolLength-1);
+      console.log("pool info: " +JSON.stringify(poolinfo));
+
+      deployedData.push({
+        id: poolLength-1,
+        implementation: imp,
+        stakingAddress: farm.address,
+        stakingToken: stakingToken,
+        rewardsAddress: poolinfo.rewardsAddress,
+        name: targetname
+      })
+    }
+
+    const shutdownPool = async (poolId) => {
+      console.log("\n\nShutdown pool " +poolId +"\n");
+      var poolinfo = await poolReg.poolInfo(poolId);
+      var wrappertoken = await ERC20.at(poolinfo.stakingToken);
+      console.log("---- pre -----");
+      console.log(JSON.stringify(poolinfo));
+      await wrappertoken.name().then(a=>console.log("token name: " +a))
+
+
+      await booster.deactivatePool(poolId,{from:deployer});
+
+
+      var poolinfo = await poolReg.poolInfo(poolId);
+      console.log("---- post -----");
+      console.log(JSON.stringify(poolinfo));
+      await wrappertoken.name().then(a=>console.log("token name: " +a))
+
+    }
+    // await deployConvexPool("0xd1f21322bBDd3586dC1151ADCcaA2684641c2b31","Convex ageur/FraxBP");
+    // await deployConvexPool("0xA0657642224Fc53dAB4a3d2069430afe157BEc5D","Convex alcx/FraxBP");
+    // await deployConvexPool("0xeC670c5e0A8A8d5ae5639158565D840DE44CA03f","Convex CVX/FraxBP");
+    // await deployConvexPool("0x57c9F019B25AaAF822926f4Cacf0a860f61eDd8D","Convex cvxCrv/FraxBP");
+    // await deployConvexPool("0x2F9504988675c91787E188Ed928D6E042d9052e9","Convex cvxFxs/FraxBP");
+    // await deployConvexPool("0xE7211E87D60177575846936F2123b5FA6f0ce8Ab","Convex dola/FraxBP");
+    // await deployConvexPool("0xdE5684F85a78F6CcCFFB4b9301ad0944eb5CE3eE","Convex mai/FraxBP");
+    // await deployConvexPool("0x40b42E4ab3c044e67CBFb0bD99C9E975dcB83668","Convex pusd/FraxBP");
+
+
+    // await deployConvexPool("0x5a92EF27f4baA7C766aee6d751f754EBdEBd9fae", "Convex Badger/FraxBP");
+    // await deployConvexPool("0xa537d64881b84faffb9Ae43c951EEbF368b71cdA", "Convex FrxEth/Eth");
+    await deployConvexPool("0xF22D3C85e41Ef4b5Ac8Cb8B89a14718e290a0561", "Convex RSR/FraxBP");
+    await deployConvexPool("0x4edF7C64dAD8c256f6843AcFe56876024b54A1b6", "Convex XAI/FraxBP");
+    await deployERC20Pool("0x73e1e624C6d3E027b8674e6C72F104F1429FC17E", "Fraxlend FRAX/FXS");
 
 
     console.log("data:");
-    console.log(deployedData);
-
-    
+    console.log(JSON.stringify(deployedData, null, 4));
     console.log("done");
   });
 });
