a04f023f3d19f00685afdea12c37d230cade4942
rickuser9600 <rsimon9600@gmail.com>
2023-04-21 23:00:21 +0900

    add tokens and sync calls for ui test script

--- contracts/test/UI_3_GetTokens.js
+++ contracts/test/UI_3_GetTokens.js
@@ -18,6 +18,8 @@ const TestPool_UniV3 = artifacts.require("TestPool_UniV3");
 const TestPool_Erc20 = artifacts.require("TestPool_Erc20");
 const ConvexPoolRegistry = artifacts.require("ConvexPoolRegistry");
 const StakingProxyConvex = artifacts.require("StakingProxyConvex");
+const IFraxFarmERC20_V2 = artifacts.require("IFraxFarmERC20_V2");
+const IConvexWrapperV2 = artifacts.require("IConvexWrapperV2");
 
 const INonfungiblePositionManager = artifacts.require("INonfungiblePositionManager");
 const IVPool = artifacts.require("IVPool");
@@ -143,6 +145,7 @@ contract("Vault Tests", async accounts => {
     let frax = await IERC20.at(contractList.frax.frax);
     let fxs = await IERC20.at(contractList.frax.fxs);
     let vefxs = await IERC20.at(contractList.frax.vefxs);
+    let crv = await IERC20.at(contractList.system.crv);
 
     let userA = accounts[0];
     let userB = accounts[1];
@@ -169,23 +172,53 @@ contract("Vault Tests", async accounts => {
 
     var actingUser = userA;
 
+    let fxsholder = "0xc8418aF6358FFddA74e09Ca9CC3Fe03Ca6aDC5b0";
+    await unlockAccount(fxsholder);
+    let crvholder = "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2";
+    await unlockAccount(crvholder);
+    let poolUtil = await PoolUtilities.at(contractList.system.poolUtility);
+
+
     //frax farm
-    // var stakingAddress = await IFraxFarmERC20_V2.at("0x4c9AD8c53d0a001E7fF08a3E5E26dE6795bEA5ac"); //eusd/fraxbp
+    var stakingAddress = await IFraxFarmERC20_V2.at("0x4c9AD8c53d0a001E7fF08a3E5E26dE6795bEA5ac"); //eusd/fraxbp
     var lptoken = await IERC20.at("0xAEda92e6A3B1028edc139A4ae56Ec881f3064D4F"); //eusd/fraxbp
     var lpHolder = "0x8605dc0C339a2e7e85EEA043bD29d42DA2c6D784"; //eusd/fraxbp
+    var stakingToken = await stakingAddress.stakingToken();
+    var stakingwrapper = await IConvexWrapperV2.at(stakingToken);
+    var distro = await stakingwrapper.distroContract();
+
+    await fxs.transfer(stakingAddress.address,web3.utils.toWei("100000.0", "ether"),{from:fxsholder,gasPrice:0});
+    console.log("fxs transfered to farm");
+    await crv.transfer(distro,web3.utils.toWei("100000.0", "ether"),{from:crvholder,gasPrice:0});
+    console.log("crv transfered to distro");
 
     await unlockAccount(lpHolder);
-    await lptoken.transfer(actingUser,web3.utils.toWei("100000.0", "ether"),{from:lpHolder,gasPrice:0});
+    await lptoken.transfer(actingUser,web3.utils.toWei("10000.0", "ether"),{from:lpHolder,gasPrice:0});
     await lptoken.balanceOf(actingUser).then(a=>console.log("lp tokens transfered: " +a) );
+    await stakingAddress.sync();
+    console.log("farm synced");
+    await poolUtil.weightedRewardRates(stakingAddress.address).then(a=>console.log("pool util -> weightedRewardRates: " +a));
 
     //STG/fraxbp
-    // var stakingAddress = await IFraxFarmERC20_V2.at("0xd600A3E4F57E718A7ad6A0cbb10c2A92c57827e6");
+    var stakingAddress = await IFraxFarmERC20_V2.at("0xd600A3E4F57E718A7ad6A0cbb10c2A92c57827e6");
     var lptoken = await IERC20.at("0x9de1c3D446237ab9BaFF74127eb4F303802a2683");
     var lpHolder = "0x8133e6B0B2420bBa10574A6668ea275f5E7Ed253";
+    var stakingToken = await stakingAddress.stakingToken();
+    var stakingwrapper = await IConvexWrapperV2.at(stakingToken);
+    var distro = await stakingwrapper.distroContract();
+
+    await fxs.transfer(stakingAddress.address,web3.utils.toWei("10000.0", "ether"),{from:fxsholder,gasPrice:0});
+    console.log("fxs transfered to farm");
+    await crv.transfer(distro,web3.utils.toWei("100000.0", "ether"),{from:crvholder,gasPrice:0});
+    console.log("crv transfered to distro");
 
     await unlockAccount(lpHolder);
     await lptoken.transfer(actingUser,web3.utils.toWei("100000.0", "ether"),{from:lpHolder,gasPrice:0});
     await lptoken.balanceOf(actingUser).then(a=>console.log("lp tokens transfered: " +a) );
+    await stakingAddress.sync();
+    console.log("farm synced");
+    await poolUtil.weightedRewardRates(stakingAddress.address).then(a=>console.log("pool util -> weightedRewardRates: " +a));
+
 
     //uzd/fraxbp
     // let stakingAddress = await IFraxFarmERC20_V2.at("0x7677D1AADcd42dC40E758115FfDE5e1E10B7f30b");
